5f62fddc3664a3b79988c38ba977f9a11be492d1
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-22 13:38:36 +0300

    feat(server): create User model with email validation

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: [true, "This Email is already exist"],
+      validate: [validator.isEmail, "Email is invalid"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.Model("User", userSchema);
